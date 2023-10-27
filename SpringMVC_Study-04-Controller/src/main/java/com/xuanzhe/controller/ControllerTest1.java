f24521f4eba43f186eaf24c72776eb16b0dc67d2
xiaxuanzhe <3058634081@qq.com>
2023-10-27 20:21:51 +0200

    Reuse of Views

--- SpringMVC_Study-04-Controller/src/main/java/com/xuanzhe/controller/ControllerTest1.java
+++ SpringMVC_Study-04-Controller/src/main/java/com/xuanzhe/controller/ControllerTest1.java
@@ -1,5 +1,18 @@
 package com.xuanzhe.controller;
 
-public class ControllerTest1 {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
 
+public class ControllerTest1 implements Controller {
+
+  @Override
+  public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
+      throws Exception {
+    ModelAndView mv = new ModelAndView();
+    mv.addObject("msg", "ControllerTest1");
+    mv.setViewName("test1");
+    return mv;
+  }
 }
